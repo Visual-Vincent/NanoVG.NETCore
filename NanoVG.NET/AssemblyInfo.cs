347d105414212e53ccedaa7f7302f67522a5fb7a
Visual-Vincent <github-vv@mydoomsite.com>
2022-01-09 12:17:03 +0100

    Initial commit

---
+++ NanoVG.NET/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿[assembly: System.Reflection.AssemblyTitle("NanoVG.NET")]
+[assembly: System.Reflection.AssemblyProduct("NanoVG.NET")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Visual Vincent 2022")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
