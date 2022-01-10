64ef548a396f92cf0b6ff27622cd91ad918d2b01
Visual-Vincent <github-vv@mydoomsite.com>
2022-01-10 21:03:58 +0100

    Implemented DllImport resolver

---
+++ NanoVG.NET/NVG.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NanoVG
+{
+    public static unsafe partial class NVG
+    {
+        static NVG()
+        {
+            NativeLibrary.SetDllImportResolver(typeof(NVG).Assembly, (name, assembly, searchPath) =>
+            {
+                if(name != LibraryName)
+                    return IntPtr.Zero;
+
+                if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return NativeLibrary.Load($"{LibraryName}.dll", assembly, searchPath);
+                }
+                else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return NativeLibrary.Load($"{LibraryName}.so", assembly, searchPath);
+                }
+                else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return NativeLibrary.Load($"{LibraryName}.dylib", assembly, searchPath);
+                }
+
+                // Unsupported platform
+                return IntPtr.Zero;
+            });
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct NVGcolor
+    {
+        public float r,g,b,a;
+    }
+}
