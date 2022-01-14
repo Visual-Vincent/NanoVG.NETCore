86cd487950c8c5036f0235e1626f4139ee28e9be
Visual-Vincent <github-vv@mydoomsite.com>
2022-01-14 20:37:52 +0100

    Implemented NuGet packaging

---
+++ nuget/pack-netcore.bat
@@ -0,0 +1,5 @@
+@echo off
+set "SolutionDir=%~dp0\..\"
+set "ProjectDir=%~dp0\..\NanoVG.NET\"
+set "ConfigurationName=Release"
+dotnet pack --no-build --no-restore -c Release ..\NanoVG.NET\NanoVG.NET.csproj
